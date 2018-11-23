405c928423126ad879400a56e5e95f874481546d
Ryusei Kurata <amnosmusic13@gmail.com>
2018-11-23 19:35:57 +0900

    [update]actionの方

--- angular/src/app/store/api/stripe.actions.ts
+++ angular/src/app/store/api/stripe.actions.ts
@@ -1,11 +1,38 @@
 import { Action } from '@ngrx/store';
 
 export enum StripeActionTypes {
-  LoadStripes = '[Stripe] Load Stripes'
+  StripeCharge = '[Stripe] Charge',
+  StripeChargeSuccess = '[Stripe] Charge Success',
+  StripeChargeFailed = '[Stripe] Charge Failed',
 }
 
-export class LoadStripes implements Action {
-  readonly type = StripeActionTypes.LoadStripes;
+export class StripeCharge implements Action {
+  readonly type = StripeActionTypes.StripeCharge;
+
+  constructor(
+    public payload: {
+
+    }
+  ) { }
 }
 
-export type StripeActions = LoadStripes;
+export class StripeChargeSuccess implements Action {
+  readonly type = StripeActionTypes.StripeChargeSuccess;
+
+  constructor(
+    public payload: {}
+  ) { }
+}
+
+export class StripeChargeFailed implements Action {
+  readonly type = StripeActionTypes.StripeChargeFailed;
+
+  constructor(
+    public payload: {}
+  ) { }
+}
+
+export type StripeActions =
+  StripeCharge
+  | StripeChargeSuccess
+  | StripeChargeFailed;
