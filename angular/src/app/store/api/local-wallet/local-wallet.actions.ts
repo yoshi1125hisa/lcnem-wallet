81118aa3c9b673b3fe7829c3c928895fbca12f63
Ryusei Kurata <amnosmusic13@gmail.com>
2018-11-23 23:46:28 +0900

    [update]Action全部定義

--- angular/src/app/store/api/local-wallet/local-wallet.actions.ts
+++ angular/src/app/store/api/local-wallet/local-wallet.actions.ts
@@ -1,9 +1,18 @@
 import { Action } from '@ngrx/store';
 
 export enum LocalWalletActionTypes {
-  LoadLocalWallets = '[LocalWallet] Load LocalWallet',
-  LoadLocalWalletsSuccess = '[LocalWallet] Load LocalWallet Success',
-  LoadLocalWalletsFailed = '[LocalWallet] Load LocalWallet Failed'
+  LoadLocalWallets = '[LocalWallet] Load LocalWallets',
+  LoadLocalWalletsSuccess = '[LocalWallet] Load LocalWallets Success',
+  LoadLocalWalletsFailed = '[LocalWallet] Load LocalWallets Failed',
+  AddLcalWallet = '[LocalWallet] Add LocalWallet',
+  AddLocalWalletSuccess = '[LocalWallet] Add LocalWallet Success',
+  AddLocalWalletFailed = '[LocalWallet] Add LocalWallet Failed',
+  UpdateLocalWallet = '[LocalWallet] Update LocalWallet',
+  UpdateLocalWalletSuccess = '[LocalWallet] Update LocalWallet Success',
+  UpdateLocalWalletFailed = '[LocalWallet] Update LocalWallet Failed',
+  DeleteLocalWallet = '[LocalWallet] Delete LocalWallet',
+  DeleteLocalWalletSuccess = '[LocalWallet] Delete LocalWallet Success',
+  DeleteLocalWalletFailed = '[LocalWallet] Delete LocalWallet Failed',
 }
 
 export class LoadLocalWallets implements Action {
@@ -32,7 +41,91 @@ export class LoadLocalWalletsFailed implements Action {
   ) { }
 }
 
+export class AddLocalWallet implements Action {
+  readonly type = LocalWalletActionTypes.AddLcalWallet;
+
+  constructor(
+    public payload: {}
+  ) { }
+}
+
+export class AddLocalWalletSuccess implements Action {
+  readonly type = LocalWalletActionTypes.AddLocalWalletSuccess;
+
+  constructor(
+    public payload: {}
+  ) { }
+}
+
+export class AddLocalWalletFailed implements Action {
+  readonly type = LocalWalletActionTypes.AddLocalWalletFailed;
+
+  constructor(
+    error: Error;
+  ) { }
+}
+
+export class UpdateLocalWallet implements Action {
+  readonly type = LocalWalletActionTypes.UpdateLocalWallet;
+
+  constructor(
+    public payload: {}
+  ) { }
+}
+
+export class UpdateLocalWalletSuccess implements Action {
+  readonly type = LocalWalletActionTypes.UpdateLocalWalletSuccess;
+
+  constructor(
+    public payload: {}
+  ) { }
+}
+
+export class UpdateLocalWalletFailed implements Action {
+  readonly type = LocalWalletActionTypes.UpdateLocalWalletFailed;
+
+  constructor(
+    error: Error;
+  ) { }
+}
+
+export class DeleteLocalWallet implements Action {
+  readonly type = LocalWalletActionTypes.DeleteLocalWallet;
+
+  constructor(
+    public payload: {}
+  ) { }
+}
+
+export class DeleteLocalWalleteSuccess implements Action {
+  readonly type = LocalWalletActionTypes.DeleteLocalWalletSuccess;
+
+  constructor(
+    public payload: {}
+  ) { }
+}
+
+export class DeleteLocalWalletFailed implements Action {
+  readonly type = LocalWalletActionTypes.DeleteLocalWalletFailed;
+
+  constructor(
+    error: Error;
+  ) { }
+}
+
+
+export class AddLocalWallets
+
 export type LocalWalletActions =
   LoadLocalWallets
   | LoadLocalWalletsSuccess
   | LoadLocalWalletsFailed
+  | AddLocalWallet
+  | AddLocalWalletSuccess
+  | AddLocalWalletFailed
+  | UpdateLocalWallet
+  | UpdateLocalWalletSuccess
+  | UpdateLocalWalletFailed
+  | DeleteLocalWallet
+  | DeleteLocalWalleteSuccess
+  | DeleteLocalWalleteFaild
