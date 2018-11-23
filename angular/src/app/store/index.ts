16ae77a8f7316f909e73d98d522b8ed18456af5b
Ryusei Kurata <amnosmusic13@gmail.com>
2018-11-23 18:59:50 +0900

    [update]reducer

--- angular/src/app/store/index.ts
+++ angular/src/app/store/index.ts
@@ -11,17 +11,20 @@ import { environment } from '../../environments/environment';
 import * as ContactReducer from './contact/contact.reducer';
 import * as WalletReducer from './wallet/wallet.reducer';
 import * as UserReducer from './user/user.reducer';
+import * as LocalWalletReducer from './api/local-wallet/local-wallet.reducer';
 
 export interface State {
   contact: ContactReducer.State,
   wallet: WalletReducer.State,
-  user: UserReducer.State
+  user: UserReducer.State,
+  localWallet: LocalWalletReducer.State
 }
 
 export const reducers: ActionReducerMap<State, any> = {
   contact: ContactReducer.reducer,
   wallet: WalletReducer.reducer,
-  user: UserReducer.reducer
+  user: UserReducer.reducer,
+  localWallet: LocalWalletReducer.reducer
 };
 
 
