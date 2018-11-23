062c8a3bcd93a3440c1c6d2f71e51e1e49d40380
Ryusei Kurata <amnosmusic13@gmail.com>
2018-11-23 19:52:04 +0900

    [update]

--- angular/src/app/store/api/stripe.reducer.ts
+++ angular/src/app/store/api/stripe.reducer.ts
@@ -1,18 +1,37 @@
 import { Action } from '@ngrx/store';
+import { StripeActions, StripeActionTypes } from './stripe.actions';
 
 
 export interface State {
-
+  loading: boolean
 }
 
 export const initialState: State = {
-
+  loading: false
 };
 
-export function reducer(state = initialState, action: Action): State {
+export function reducer(
+  state = initialState,
+  action: StripeActions): State {
   switch (action.type) {
-
+    case StripeActionTypes.StripeCharge: {
+      return {
+        ...state,
+        loading: true
+      }
+    }
+    case StripeActionTypes.StripeChargeSuccess: {
+      return {
+        ...state,
+        loading: false
+      }
+    }
+    case StripeActionTypes.StripeChargeFailed: {
+      return {
+        ...state,
+        loading: false
+      }
+    }
     default:
       return state;
   }
-}
