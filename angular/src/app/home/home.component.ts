8d67be6157751a7bd540d6999409491a3bf5dbe3
Ryotaro Miyagwa <o_o_o_o_o1031@yahoo.co.jp>
2018-11-26 17:35:17 +0900

    コンポーネントの作成

--- angular/src/app/home/home.component.ts
+++ angular/src/app/home/home.component.ts
@@ -4,7 +4,7 @@ import { Asset, NEMLibrary, NetworkTypes } from 'nem-library';
 import { Observable, of } from 'rxjs';
 import { map, mergeMap, first } from 'rxjs/operators';
 import { Store } from '@ngrx/store';
-import { State } from '../store/index'
+import { State } from '../store/index';
 import { Logout } from '../store/user/user.actions';
 import { LoadWallets } from '../store/wallet/wallet.actions';
 import { Wallet } from '../store/wallet/wallet.model';
