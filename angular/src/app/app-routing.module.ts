81e2cc124fdd2aef271072f73274fe98b0998d2f
Yu Kimura <33382781+YuKimura45z@users.noreply.github.com>
2018-12-03 16:37:31 +0900

    [update]appmodule

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -1,31 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './accounts/login/login.component';
-import { ScanComponent } from './transactions/scan/scan.component';
-import { TransferComponent } from './transactions/transfer/transfer.component';
+import { PagesComponent } from './pages/pages.component';
 import { PageNotFoundComponent } from './error/page-not-found/page-not-found.component';
-import { TermsComponent } from './terms/terms/terms.component';
-import { DepositComponent } from './accounts/deposit/deposit.component';
-import { WithdrawComponent } from './accounts/withdraw/withdraw.component';
-import { ContactsComponent } from './accounts/contacts/contacts.component';
-import { PrivacyPolicyComponent } from './terms/privacy-policy/privacy-policy.component';
-import { PlanComponent } from './accounts/plan/plan.component';
-import { WalletsComponent } from './accounts/wallets/wallets.component';
-
 
 const routes: Routes = [
-  { path: "", component: HomeComponent },
-  { path: "accounts/login", component: LoginComponent },
-  { path: "accounts/wallets", component: WalletsComponent },
-  { path: "accounts/plan", component: PlanComponent },
-  { path: "accounts/deposit", component: DepositComponent },
-  { path: "accounts/withdraw", component: WithdrawComponent },
-  { path: "accounts/contacts", component: ContactsComponent },
-  { path: "transactions/scan", component: ScanComponent },
-  { path: "transactions/transfer", component: TransferComponent },
-  { path: "terms/terms", component: TermsComponent },
-  { path: "terms/privacy-policy", component: PrivacyPolicyComponent },
+  { path: "", component: PagesComponent },
   { path: "**", component: PageNotFoundComponent }
 ];
 
