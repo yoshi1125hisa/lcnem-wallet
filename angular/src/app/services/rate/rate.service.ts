fd5ea2e0f5729f779b4da3b0b02bc409898abbab
Ryusei Kurata <amnosmusic13@gmail.com>
2018-12-22 21:26:04 +0900

    [update]rateServiceのクラス設定

--- angular/src/app/services/rate/rate.service.ts
+++ angular/src/app/services/rate/rate.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { RxEffectiveStateStore } from 'rx-state-store-js';
 
 @Injectable({
   providedIn: 'root'
 })
-export class RateService {
+export class RateService extends RxEffectiveStateStore<State> {
 
-  constructor() { }
+  constructor() {
+    super(
+      {
+        current: "USD",
+        usd: 1
+      }
+    )
+  }
+}
+
+
+interface State {
+  current: string
+  xem?: number
+  btc?: number
+  eht?: number
+  jpy?: number
+  usd: 1
 }
