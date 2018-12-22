0d77b5318e17b9914aec9f11c1aac9229d31200f
Ryusei Kurata <amnosmusic13@gmail.com>
2018-12-22 21:42:45 +0900

    [update]service作成

--- angular/src/app/services/rate/rate.service.ts
+++ angular/src/app/services/rate/rate.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { RxEffectiveStateStore } from 'rx-state-store-js';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { Rate } from '../../../../../firebase/functions/src/models/rate'
 
 
 @Injectable({
@@ -14,22 +15,21 @@ export class RateService extends RxEffectiveStateStore<State> {
     super(
       {
         current: "USD",
-        usd: 1
       }
     )
   }
-  public loadRate() {
-    this.firestore.collection("rates").get().subscribe(
+  public loadRate(current: string) {
+    this.firestore.doc("rates").get().subscribe(
       (document) => {
         const state: State = {
+          current: current,
           rate: document.data() as Rate
         }
         this.streamState(state)
       },
       (error) => {
         this.streamErrorState(error)
-      }
-    )
+      })
   }
 }
 
