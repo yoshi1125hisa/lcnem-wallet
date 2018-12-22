05ba036e9a506d131da533c508a5170fe1863e2a
Ryusei Kurata <amnosmusic13@gmail.com>
2018-12-22 21:32:11 +0900

    [update]streamの型作成

--- angular/src/app/services/rate/rate.service.ts
+++ angular/src/app/services/rate/rate.service.ts
@@ -1,16 +1,33 @@
 import { Injectable } from '@angular/core';
 import { RxEffectiveStateStore } from 'rx-state-store-js';
+import { AngularFirestore } from '@angular/fire/firestore';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class RateService extends RxEffectiveStateStore<State> {
 
-  constructor() {
+  constructor(
+    private firestore: AngularFirestore
+  ) {
     super(
       {
         current: "USD",
         usd: 1
+      }
+    )
+  }
+  public loadRate() {
+    this.firestore.collection("rates").get().subscribe(
+      (document) => {
+        const state: State = {
+          rate: document.data() as Rate
+        }
+        this.streamState(state)
+      },
+      (error) => {
+        this.streamErrorState(error)
       }
     )
   }
