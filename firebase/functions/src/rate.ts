d209a7ea1cdd7ad5b1735145ee3bd64a5361e35d
Ryusei Kurata <amnosmusic13@gmail.com>
2018-12-23 23:15:08 +0900

    [update]indent

--- firebase/functions/src/rate.ts
+++ firebase/functions/src/rate.ts
@@ -3,47 +3,47 @@ import * as request from 'request';
 import * as admin from 'firebase-admin';
 
 export const _rate = functions.https.onRequest((req, res) => {
-    try {
-        request.get(
-            'http://apilayer.net/api/' + 'live' + '?access_key=' + functions.config().currency_layer.api_key + 'format=1',
-            (error, res, body) => {
-                const jsonResponse = JSON.parse(body)
-                const jpy = jsonResponse.quotes.USDJPY
-                admin.firestore().collection("rates").doc("rateId").set({
-                    rates: [{ "JPY": jpy }, { "USD": 1 }]
-                })
-            }
-        )
+  try {
+    request.get(
+      'http://apilayer.net/api/' + 'live' + '?access_key=' + functions.config().currency_layer.api_key + 'format=1',
+      (error, res, body) => {
+        const jsonResponse = JSON.parse(body)
+        const jpy = jsonResponse.quotes.USDJPY
+        admin.firestore().collection("rates").doc("rateId").set({
+          rates: [{ "JPY": jpy }, { "USD": 1 }]
+        })
+      }
+    )
 
-        const btc = "1";
-        const xem = "873";
-        const eth = "1027";
-        const ids = [btc, xem, eth]
+    const btc = "1";
+    const xem = "873";
+    const eth = "1027";
+    const ids = [btc, xem, eth]
 
-        ids.map(
-            id =>
-                request.get({
-                    uri: 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?id=' + id,
-                    headers: {
-                        'X-CMC_PRO_API_KEY': functions.config().coin_market_cap.api_key
-                    }
-                }, function (error, res, body) {
-                    if (error) {
-                    } else {
-                        var jsonResponse = JSON.parse(body);
-                        var name = String(jsonResponse.data.id.name)
-                        var price = jsonResponse.data.id.quote.USD.price;
-                        var rate = {}
-                        rate[`${name}`] = price
+    ids.map(
+      id =>
+        request.get({
+          uri: 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?id=' + id,
+          headers: {
+            'X-CMC_PRO_API_KEY': functions.config().coin_market_cap.api_key
+          }
+        }, function (error, res, body) {
+          if (error) {
+          } else {
+            var jsonResponse = JSON.parse(body);
+            var name = String(jsonResponse.data.id.name)
+            var price = jsonResponse.data.id.quote.USD.price;
+            var rate = {}
+            rate[`${name}`] = price
 
-                        admin.firestore().collection("rates").doc("rateId").set({
-                            rates: rate
-                        })
-                    }
-                })
-        )
-    } catch (e) {
-        console.error(e)
-        res.status(400).send(e.message)
-    }
+            admin.firestore().collection("rates").doc("rateId").set({
+              rates: rate
+            })
+          }
+        })
+    )
+  } catch (e) {
+    console.error(e)
+    res.status(400).send(e.message)
+  }
 });
