4fea69895d5a4abf45807bfe9dc4fe2787723344
Ryusei Kurata <amnosmusic13@gmail.com>
2018-12-23 23:03:35 +0900

    [update]functions

--- firebase/functions/src/rate.ts
+++ firebase/functions/src/rate.ts
@@ -5,17 +5,19 @@ import * as admin from 'firebase-admin';
 export const _rate = functions.https.onRequest((req, res) => {
     try {
         request.get(
-            'http://data.fixer.io/api/' + 'latest' + functions.config().fixer.api_access_key,
+            'http://data.fixer.io/api/' + 'latest' + '?access_key=' + functions.config().fixer.api_access_key,
             (error, res, body) => {
                 const jsonResponse = JSON.parse(body)
-                jsonResponse.data.findindex
+                const jpy = jsonResponse.data.findindex
+                admin.firestore().collection("rates").doc("rateId").set({
+                    rates: [{ "JPY": jpy }, { "USD": 1 }]
+                })
             }
         )
 
         const btc = "1";
         const xem = "873";
         const eth = "1027";
-
         const ids = [btc, xem, eth]
 
         ids.map(
@@ -29,11 +31,13 @@ export const _rate = functions.https.onRequest((req, res) => {
                     if (error) {
                     } else {
                         var jsonResponse = JSON.parse(body);
-                        var name = jsonResponse.data.id.name
+                        var name = String(jsonResponse.data.id.name)
                         var price = jsonResponse.data.id.quote.USD.price;
-                        var objectRiteral = [name, price]
-                        admin.firestore().doc("rates").set({
-                            rate: objectRiteral
+                        var rate = {}
+                        rate[`${name}`] = price
+
+                        admin.firestore().collection("rates").doc("rateId").set({
+                            rates: rate
                         })
                     }
                 })
