bf6e9b6805a9bcbd7a8392a93e30d76036d1cf3e
Ryusei Kurata <amnosmusic13@gmail.com>
2018-12-31 15:06:07 +0900

    [update]

--- firebase/functions/src/rate.ts
+++ firebase/functions/src/rate.ts
@@ -4,54 +4,53 @@ import * as admin from 'firebase-admin';
 
 export const _rate = functions.https.onRequest((req, res) => {
   try {
-    const jpy = new Promise(
+    const jpyPromise = new Promise(
       function (resolve, reject) {
-        resolve(
-          request.get(
-            'http://apilayer.net/api/' + 'live' + '?access_key=' + functions.config().currency_layer.api_key + 'format=1',
-            function (error, res, body) {
-              const jsonResponse = JSON.parse(body)
-              return { "JPY": 1 / jsonResponse['quotes']['USDJPY'] }
-            }
-          )
+        request.get(
+          'http://apilayer.net/api/' + 'live' + '?access_key=' + functions.config().currency_layer.api_key + 'format=1',
+          function (error, res, body) {
+            const jsonResponse = JSON.parse(body)
+            resolve({ "JPY": 1 / jsonResponse['quotes']['USDJPY'] })
+          }
         )
       }
     )
 
-    const btc = "1";
-    const xem = "873";
-    const eth = "1027";
-    const ids = [btc, xem, eth]
-    const others = ids.map<{}>(
+    const enum CryptIds {
+      BTC = "1",
+      XEM = "873",
+      ETH = "1027"
+    }
+    const cryptIds = [CryptIds.BTC, CryptIds.XEM, CryptIds.ETH]
+    const cryptPromisses = cryptIds.map<{}>(
       async (id) => {
         await new Promise(
           function (resolve, reject) {
-            resolve(
-              request.get(
-                {
-                  uri: 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?id=' + id,
-                  headers: {
-                    'X-CMC_PRO_API_KEY': functions.config().coin_market_cap.api_key
-                  }
-                }, function (error, res, body) {
-                  if (error) {
-                  } else {
-                    const jsonResponse = JSON.parse(body);
-                    const name = String(jsonResponse['data'][`${id}`]['name'])
-                    const price = jsonResponse['data'][`${id}`]['quote']['USD']['price'];
-                    const rate = {}
-                    rate[`${name}`] = price
-                  }
+            request.get(
+              {
+                uri: 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?id=' + id,
+                headers: {
+                  'X-CMC_PRO_API_KEY': functions.config().coin_market_cap.api_key
                 }
-              )
+              }, function (error, res, body) {
+                if (error) {
+                } else {
+                  const jsonResponse = JSON.parse(body);
+                  const name = String(jsonResponse['data'][`${id}`]['name'])
+                  const price = jsonResponse['data'][`${id}`]['quote']['USD']['price'];
+                  const rate = {}
+                  resolve(rate[`${name}`] = price)
+                }
+              }
             )
           }
         )
       }
     )
-    others.push(jpy)
+    const ratePromisses = cryptPromisses
+    ratePromisses.push(jpyPromise)
 
-    Promise.all(others).then(async function (message) {
+    Promise.all(ratePromisses).then(async function (message) {
       var obj = Object()
       message.forEach(function (el) {
         obj.assign(el)
