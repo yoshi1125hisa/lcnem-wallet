9739213f3d41fce8dffe0705a18273e116c90a85
Yu Kimura <33382781+YuKimura45z@users.noreply.github.com>
2019-01-01 20:06:08 +0900

    [update]router

--- angular/src/app/services/router/router.service.ts
+++ angular/src/app/services/router/router.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Router, NavigationExtras, NavigationStart } from '@angular/router';
 import { AuthService } from '../auth/auth.service';
 import { WalletService } from '../wallet/wallet.service';
-import { filter, first } from 'rxjs/operators';
+import { filter, first, map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -14,33 +14,30 @@ export class RouterService {
     private auth: AuthService,
     private wallet: WalletService
   ) {
-    this.router.events.subscribe(
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationStart),
+      map(event => event as NavigationStart)
+    ).subscribe(
       async (event) => {
-        if (event instanceof NavigationStart) {
-          switch (event.url) {
-            case "/account/login": {
-              break
-            }
-            case "/account/wallets": {
-              const user = await this.auth.user$.pipe(first()).toPromise()
-              if (!user) {
-                this.router.navigate(["account", "login"])
-                return
-              }
-              break
-            }
-            default: {
-              const state = await this.wallet.state$.pipe(
-                filter(state => !state.loading),
-                first()
-              ).toPromise()
-              
-              if (!state.currentWalletId) {
-                this.router.navigate(["account", "wallets"])
-              }
-              break
-            }
-          }
+        if (event.url === "/account/login") {
+          return
+        }
+        const user = await this.auth.user$.pipe(first()).toPromise()
+        if (!user) {
+          this.router.navigate(["account", "login"])
+          return
+        }
+
+        if (event.url === "/account/wallets") {
+          return
+        }
+        const state = await this.wallet.state$.pipe(
+          filter(state => !state.loading),
+          first()
+        ).toPromise()
+
+        if (!state.currentWalletId) {
+          this.router.navigate(["account", "wallets"])
         }
       }
     )
