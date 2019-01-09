d09a824c5651d52ecbc72ff1a52cea7474435a84
Yu Kimura <33382781+YuKimura45z@users.noreply.github.com>
2019-01-09 15:42:49 +0900

    [update]

--- angular/src/app/services/api/stripe/stripe.service.ts
+++ angular/src/app/services/api/stripe/stripe.service.ts
@@ -1,13 +1,9 @@
 import { Injectable } from '@angular/core';
-import { environment } from '../../../../environments/environment';
-import 'firebase/auth';
-import { AuthService } from '../../../services/auth/auth.service';
 import { AngularFirestore } from '@angular/fire/firestore';
-import { database } from 'firebase';
-import { stripeCharge, supportedInstruments } from '../../../../stripe';
+import { environment } from '../../../../environments/environment';
+import { AuthService } from '../../../services/auth/auth.service';
 
 declare const Stripe: any;
-
 
 @Injectable({
   providedIn: 'root'
@@ -17,34 +13,37 @@ export class StripeService {
   constructor(
     private auth: AuthService,
     private firestore: AngularFirestore,
-  ) { console.log("stripe"); }
+  ) { }
 
   public async charge() {
-
     Stripe.setPublishableKey(environment.stripe.pk);
 
     const uid = this.auth.user!.uid;
 
-    const supportedInstruments = [{
-      supportedMethods: ['basic-card'],
-      data: {
-        supportedNetworks: [
-          'visa',
-          'mastercard'
-        ]
+    const supportedInstruments = [
+      {
+        supportedMethods: ['basic-card'],
+        data: {
+          supportedNetworks: [
+            'visa',
+            'mastercard'
+          ]
+        }
       }
-    }]
+    ]
 
     let details = {
-      displayItems: [{
-        label: 'LCNEM Wallet Standard Plan Fee Per Month',
-        amount: { currency: 'JPY', value: '200' }
-      }],
+      displayItems: [
+        {
+          label: 'LCNEM Wallet Standard Plan Fee Per Month',
+          amount: { currency: 'JPY', value: '200' }
+        }
+      ],
       total: {
         label: 'LCNEM Wallet Standard Plan Total Fee Per Month',
         amount: { currency: 'JPY', value: '200' }
       }
-    };
+    }
 
     const request = new PaymentRequest(supportedInstruments, details, { requestShipping: false });
 
@@ -53,46 +52,53 @@ export class StripeService {
       return;
     }
 
-    const token: any = Stripe.card.createToken({
-      number: result.details.cardNumber,
-      cvc: result.details.cardSecurityCode,
-      exp_month: result.details.expiryMonth,
-      exp_year: result.details.expiryYear
-    });
-
-    Stripe.Customer.create({
-      email: this.auth.user!.email,
-      source: token,
-      id: uid,
-    }, (err: any, customer: any) => {
-
-      if (!err && customer) {
-        Stripe.subscriptions.create({
-          customer: customer.id,
-          trial_end: this.get1stDayOfNextMonth().getTime() / 1000,
-          items: [{ plan: "plan_EFWnnrtQXB3tR6" }]
-        }, (err: any, subscription: any) => {
-          if (!err && subscription) {
-            this.firestore.collection("users").doc(uid).set({
-              plan: "Standard",
-              subscription_id: subscription.id
-            }, { merge: true });
-            return JSON.stringify({ message: "OK" });
-          } else {
-            return JSON.stringify({ message: err });
-          }
-        });
-      } else {
-        return JSON.stringify({ message: err });
+    const token: any = Stripe.card.createToken(
+        {
+        number: result.details.cardNumber,
+        cvc: result.details.cardSecurityCode,
+        exp_month: result.details.expiryMonth,
+        exp_year: result.details.expiryYear
       }
-    }
-    );
+    )
+
+    Stripe.Customer.create(
+      {
+        email: this.auth.user!.email,
+        source: token,
+        id: uid,
+      },
+      (err: any, customer: any) => {
+        if (!err && customer) {
+          Stripe.subscriptions.create(
+            {
+              customer: customer.id,
+              trial_end: this.getFirstDayOfNextMonth().getTime() / 1000,
+              items: [{ plan: "plan_EFWnnrtQXB3tR6" }]
+            },
+            (err: any, subscription: any) => {
+              if (!err && subscription) {
+                this.firestore.collection("users").doc(uid).set({
+                  plan: "Standard",
+                  subscription_id: subscription.id
+                }, { merge: true });
+                return JSON.stringify({ message: "OK" });
+              } else {
+                return JSON.stringify({ message: err });
+              }
+            }
+          )
+        } else {
+          return JSON.stringify({ message: err });
+        }
+      }
+    )
   }
 
-  private get1stDayOfNextMonth(): Date {
+  private getFirstDayOfNextMonth(): Date {
     const date = new Date();
     date.setDate(1);
     date.setMonth(date.getMonth() + 1);
+
     return date;
   }
 }
--- firebase/functions/src/models/permission.ts
+++
@@ -1,9 +0,0 @@
-import { Timestamp } from "@google-cloud/firestore";
-
-export interface Permission {
-  userId: string
-  walletId: string
-  accessToken: string
-  refreshToken: string
-  expire: Timestamp
-}
