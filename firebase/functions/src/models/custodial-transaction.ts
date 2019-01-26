fd5f8699aa11f1aef3b0bf46ac093b674032379f
unknown <33382781+YuKimura45z@users.noreply.github.com>
2019-01-26 20:01:53 +0900

    [update]

--- firebase/functions/src/models/custodial-transaction.ts
+++ firebase/functions/src/models/custodial-transaction.ts
@@ -1,9 +1,26 @@
+import * as admin from 'firebase-admin';
+
 export interface CustodialTransaction {
-  userId: string | "admin"
   recipientId: string | "admin"
   amount: number
 }
 
 export function sendCustodialLightning(userId: string | "admin", recipientId: string | "admin", amount: number) {
-  
+  if (userId !== "admin") {
+    admin.firestore().collection("users").doc(userId).collection("custodies").doc("lightning").collection("transactions").add(
+      {
+        recipientId: recipientId,
+        amount: amount
+      } as CustodialTransaction
+    )
+  }
+
+  if(recipientId !== "admin") {
+    admin.firestore().collection("users").doc(recipientId).collection("custodies").doc("lightning").collection("transactions").add(
+      {
+        recipientId: userId,
+        amount: -1 * amount
+      } as CustodialTransaction
+    )
+  }
 }
