9500c65b67b4f61a977e30f42de7d33fbe2dd675
unknown <33382781+YuKimura45z@users.noreply.github.com>
2019-01-27 15:16:21 +0900

    [update]lightning-bitcoin

--- angular/src/app/services/dlt/bitcoin/balance/balance.service.ts
+++ angular/src/app/services/dlt/bitcoin/balance/balance.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { RxEffectiveStateStore, RxEffectiveState } from 'rx-state-store-js';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { HttpClient } from '@angular/common/http';
+import { forkJoin } from 'rxjs';
+import { CustodialTransaction } from '../../../../../../../firebase/functions/src/models/custodial-transaction';
 
 @Injectable({
   providedIn: 'root'
@@ -8,6 +11,7 @@ import { AngularFirestore } from '@angular/fire/firestore';
 export class BalanceService extends RxEffectiveStateStore<State> {
 
   constructor(
+    private http: HttpClient,
     private firestore: AngularFirestore
   ) {
     super(
@@ -19,7 +23,31 @@ export class BalanceService extends RxEffectiveStateStore<State> {
     )
   }
 
-  public loadBalance(address: string, refresh?: boolean) {
+  public loadBalance(address: string, userId: string, refresh?: boolean) {
+    if(this.state.lastAddress === address && this.state.lastUserId === userId && !refresh) {
+      return
+    }
+    this.streamLoadingState()
+
+    forkJoin(
+      this.http.get(`https://api.blockcypher.com/v1/btc/main/addrs/${address}/balance`),
+      this.firestore.collection("users").doc(userId).collection("custodies").doc("lightning").collection("transactions").get()
+    ).subscribe(
+      ([data, transactions]) => {
+        const state: State = {
+          amount: Number((data as any).balance),
+          custodialLightningAmount: transactions.docs.map(doc => doc.data() as CustodialTransaction).map(t => t.amount).reduce((a, b) => a+ b),
+          lastAddress: address,
+          lastUserId: userId,
+          loading: false
+        }
+
+        this.streamState(state)
+      },
+      (error) => {
+        this.streamErrorState(error)
+      }
+    )
   }
 }
 
@@ -27,4 +55,5 @@ interface State extends RxEffectiveState {
   amount: number
   custodialLightningAmount: number
   lastAddress?: string
+  lastUserId?: string
 }
