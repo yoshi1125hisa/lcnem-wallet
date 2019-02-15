771ca8a61f9bc0f642a4a6570a01174033fd1d59
unknown <33382781+YuKimura45z@users.noreply.github.com>
2019-02-15 18:42:51 +0900

    [update]effect

--- angular/src/app/services/user/application/application.effects.ts
+++ angular/src/app/services/user/application/application.effects.ts
@@ -1,18 +1,22 @@
 import { Injectable } from '@angular/core';
 import { Actions, Effect, ofType } from '@ngrx/effects';
-
-
-
 import { ApplicationActionTypes } from './application.actions';
+import * as fromApplication from './application.reducer';
+import { Store } from '@ngrx/store';
 
 @Injectable()
 export class ApplicationEffects {
 
 
   @Effect()
-  loadApplications$ = this.actions$.pipe(ofType(ApplicationActionTypes.LoadApplications));
+  loadApplications$ = this.actions$.pipe(
+    ofType(ApplicationActionTypes.LoadApplications)
+  );
 
 
-  constructor(private actions$: Actions) {}
+  constructor(
+    private actions$: Actions,
+    private store: Store<fromApplication.State>
+  ) {}
 
 }
--- angular/src/app/services/user/contact/contact.effects.ts
+++ angular/src/app/services/user/contact/contact.effects.ts
@@ -1,18 +1,22 @@
 import { Injectable } from '@angular/core';
 import { Actions, Effect, ofType } from '@ngrx/effects';
-
-
-
 import { ContactActionTypes } from './contact.actions';
+import * as fromContact from './contact.reducer';
+import { Store } from '@ngrx/store';
 
 @Injectable()
 export class ContactEffects {
 
 
   @Effect()
-  loadContacts$ = this.actions$.pipe(ofType(ContactActionTypes.LoadContacts));
+  loadContacts$ = this.actions$.pipe(
+    ofType(ContactActionTypes.LoadContacts)
+  );
 
 
-  constructor(private actions$: Actions) {}
+  constructor(
+    private actions$: Actions,
+    private store: Store<fromContact.State>
+  ) {}
 
 }
